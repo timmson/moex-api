a7c78710f854f64c1fe9da1bbffaf79149a2e7e9
timmson <timmson666@mail.ru>
2021-01-29 18:03:48 +0300

    Fix main script

--- test/test-array_combine.js
+++ test/test-array_combine.js
@@ -1,5 +1,5 @@
 const array_comnine = require("../lib/array_combine");
 
 test("array_combine", () => {
-    expect(array_comnine([1, 2], [3, 4])).toEqual({"1": 3, "2": 4});
+	expect(array_comnine([1, 2], [3, 4])).toEqual({"1": 3, "2": 4});
 });
